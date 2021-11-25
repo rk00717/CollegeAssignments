54b5f22f84b5f5444f26d7cefaa17cecff9f258f
rk00717 <58892434+rk00717@users.noreply.github.com>
2021-11-25 17:09:45 +0530

    ScriptChange

--- Assignement_1/Assets/Scripts/ParallaxBG.cs
+++ Assignement_1/Assets/Scripts/ParallaxBG.cs
@@ -6,7 +6,7 @@ public class ParallaxBG : MonoBehaviour{
     [SerializeField]
     private Transform cameraPos;
     [SerializeField]
-    [Range(0, 1)]
+    [Range(-1, 0)]
     private float parallaxEffect;
     private float initialPos;
     private float spriteLength;
@@ -14,7 +14,7 @@ public class ParallaxBG : MonoBehaviour{
     private float offsetParam;
     
     void Start(){
-        parallaxEffect *= -1;
+        // parallaxEffect *= -1;
         initialPos = cameraPos.position.x;
         spriteLength = GetComponent<SpriteRenderer>().bounds.size.x - offsetParam;
     }
@@ -24,9 +24,6 @@ public class ParallaxBG : MonoBehaviour{
         float distance = cameraPos.position.x * parallaxEffect;
 
         transform.position = new Vector3(initialPos + distance, 0f, 0f);
-
-        // if(cameraPos.position.x > transform.position.x + spriteLength) initialPos += spriteLength;
-        // else if(cameraPos.position.x < transform.position.x - spriteLength) initialPos -= spriteLength;
 
         if(tempPos > initialPos + spriteLength) initialPos += spriteLength;
         else if(tempPos < initialPos - spriteLength) initialPos -= spriteLength;
